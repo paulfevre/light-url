9d5265a4dbc0f58d0f96a9f7bd7e30a6c23b3be8
Paul FEVRE <git@paul-fevre.com>
2015-03-11 22:53:10 +0100

    Redirect route and Twig installation

--- src/controller.php
+++ src/controller.php
@@ -1,5 +1,9 @@
 <?php
 
-$app->match('/', function() use ($app) {
+$app->get('/', function() use ($app) {
     return $app['twig']->render('index.html.twig');
 })->bind('index');
+
+$app->get('/{alias}', function($alias) use ($app) {
+    return $app['twig']->render('redirect.html.twig', array('alias' => $alias));
+})->bind('redirect');
