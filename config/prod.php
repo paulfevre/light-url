566e3ee302a84c15981c0b5cbf2da425da176b9c
Paul FEVRE <git@paul-fevre.com>
2015-03-11 01:45:41 +0100

    Add config file

---
+++ config/prod.php
@@ -0,0 +1,10 @@
+<?php
+
+// Doctrine
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'dbname'   => 'light-url',
+    'user'     => 'root',
+    'password' => '',
+);
--- web/app.php
+++ web/app.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require __DIR__ . '/../config/prod.php';
 require __DIR__ . '/../src/app.php';
 require __DIR__ . '/../src/controller.php';
 
